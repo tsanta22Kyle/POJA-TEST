f1acdbcc5339c8fd13af65bc1f2cbdaa32c63370
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-16 15:15:53 +0300

    fix : merge preprod

--- src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
+++ src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
@@ -12,7 +12,7 @@ public class SimpleVarEnvTest extends FacadeIT {
 
   @Test
   public void test() {
-    String expectedKey = "sk_preprod_test";
+    String expectedKey = "sk_prod_test";
     String actualKey = subject.getSecretKey();
     Assertions.assertEquals(expectedKey, actualKey, "💥 expected=" + expectedKey + " / actual=" + actualKey);
   }

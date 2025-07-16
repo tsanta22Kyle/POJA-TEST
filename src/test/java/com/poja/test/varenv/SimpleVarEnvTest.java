66d3a87ea7a5722ecfabf87a22af3693ff35efe8
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-16 15:20:56 +0300

    fix : format

--- src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
+++ src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
@@ -14,6 +14,7 @@ public class SimpleVarEnvTest extends FacadeIT {
   public void test() {
     String expectedKey = "sk_prod_test";
     String actualKey = subject.getSecretKey();
-    Assertions.assertEquals(expectedKey, actualKey, "💥 expected=" + expectedKey + " / actual=" + actualKey);
+    Assertions.assertEquals(
+        expectedKey, actualKey, "💥 expected=" + expectedKey + " / actual=" + actualKey);
   }
 }

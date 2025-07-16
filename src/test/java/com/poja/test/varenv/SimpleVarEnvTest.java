cd9c95709e6b5bf81194902292e5acb7a72d95e1
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-16 14:54:41 +0300

    fix : add logs

--- src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
+++ src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
@@ -14,6 +14,8 @@ public class SimpleVarEnvTest extends FacadeIT {
   public void test() {
     String expectedKey = "sk_prod_test";
     String actualKey = subject.getSecretKey();
+    System.out.println("🔐 expected = " + expectedKey);
+    System.out.println("📦 actual   = " + actualKey);
     Assertions.assertEquals(expectedKey, actualKey);
   }
 }

af26e35c803c17757b7a06a013e8cb93ff477ff2
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-16 13:15:18 +0300

    feat : simple env vars tests

---
+++ src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
@@ -0,0 +1,23 @@
+package com.poja.test.varenv;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SimpleVarEnvTest {
+
+    @Test
+    public void test() {
+        String expectedKey = "sk_preprod_test";
+        String actualKey = System.getenv("SECRET_KEY");
+
+        Assertions.assertEquals(expectedKey, actualKey);
+    }
+
+
+}

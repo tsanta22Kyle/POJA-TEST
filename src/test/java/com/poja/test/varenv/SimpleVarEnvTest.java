0ab53789a462548577ba194a671ed0f02fd6ef04
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-16 15:14:02 +0300

    fix : add logs in asser values

--- src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
+++ src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
@@ -12,10 +12,8 @@ public class SimpleVarEnvTest extends FacadeIT {
 
   @Test
   public void test() {
-    String expectedKey = "sk_prod_test";
+    String expectedKey = "sk_preprod_test";
     String actualKey = subject.getSecretKey();
-    System.out.println("🔐 expected = " + expectedKey);
-    System.out.println("📦 actual   = " + actualKey);
-    Assertions.assertEquals(expectedKey, actualKey);
+    Assertions.assertEquals(expectedKey, actualKey, "💥 expected=" + expectedKey + " / actual=" + actualKey);
   }
 }

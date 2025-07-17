e868fe16a2ece2dfd049e7071d5c5d162d8ccbff
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-17 14:18:03 +0300

    fix : prod expected value change

--- src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
+++ src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
@@ -14,7 +14,7 @@ public class SimpleVarEnvTest extends FacadeIT {
   @Test
   @Disabled
   public void test() {
-    String expectedKey = "sk_preprod_test";
+    String expectedKey = "sk_prod_test";
     String actualKey = subject.getSecretKey();
     Assertions.assertEquals(expectedKey, actualKey);
   }

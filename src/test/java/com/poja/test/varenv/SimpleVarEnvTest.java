22c2c4216e3dcc50b58fe634416be71d88688205
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-17 14:20:42 +0300

    fix : enable test

--- src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
+++ src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
@@ -3,7 +3,6 @@ package com.poja.test.varenv;
 import com.poja.test.conf.FacadeIT;
 import com.poja.test.service.SecretService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,7 +11,6 @@ public class SimpleVarEnvTest extends FacadeIT {
   @Autowired private SecretService subject;
 
   @Test
-  @Disabled
   public void test() {
     String expectedKey = "sk_preprod_test";
     String actualKey = subject.getSecretKey();

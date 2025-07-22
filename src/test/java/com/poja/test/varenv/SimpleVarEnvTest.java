59708c80cde2a1d72bc38a32bb5d7a0b5d10cfee
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-22 14:41:58 +0300

    fix : re change value & add not null assertion

--- src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
+++ src/test/java/com/poja/test/varenv/SimpleVarEnvTest.java
@@ -12,8 +12,9 @@ public class SimpleVarEnvTest extends FacadeIT {
 
   @Test
   public void test() {
-    String expectedKey = "sk_prod_test";
+    String expectedKey = "sk_preprod_test";
     String actualKey = subject.getSecretKey();
     Assertions.assertEquals(expectedKey, actualKey);
+    Assertions.assertNotNull(actualKey);
   }
 }
